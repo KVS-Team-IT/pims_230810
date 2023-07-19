c1488f5c2e9165105be7839ee0c4ef32914160ce
kvsteamit <kvsteamit@gmail.com>
2023-07-19 16:47:04 +0530

    230719

--- application/modules/app/controllers/Home.php
+++ application/modules/app/controllers/Home.php
@@ -1,10 +1,22 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
+<?php if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 class Home extends MY_Controller
 {
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('home_model');
+    }
+    //========================= WELCOME TO PIMS HOME =====================//
     public function index()
     {
-        $this->load->view('welcome_message');
+        $PIMS_DATA['UD'] = $this->home_model->LogMasterData($this->auth_user_id, $this->role_id);
+        $data = array(
+            'title' => WEBSITE_TITLE . ' - HOME',
+            'javascripts' => array(),
+            'style_sheets' => array(),
+            'content' => $this->load->view('Home', (isset($PIMS_DATA)) ? $PIMS_DATA : '', TRUE)
+        );
+        $this->load->view($this->template, $data);
     }
 }
